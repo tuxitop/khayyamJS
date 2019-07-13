852a29aa12da5e7c06cbc96d0e222a03b3cd0830
Ali Mousavi <ali.mousavi@gmail.com>
2019-07-13 19:06:04 +0430

    Update license year

--- khayyamJS.user.js
+++ khayyamJS.user.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Ali Mousavi
+Copyright (c) 2019 Ali Mousavi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
